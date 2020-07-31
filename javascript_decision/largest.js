85ee8e5f7174cb6caf1a4eb6ee16605b1a576167
Jibinjoychan <jibinjoychan@gmail.com>
2020-07-31 10:58:40 +0530

    javascript-decision

---
+++ javascript_decision/largest.js
@@ -0,0 +1,18 @@
+var n1=prompt("enter number 1");
+var n2=prompt("enter number 2");
+var n3=prompt("enter number 3");
+if (n1>=n2 && n1>=n3)
+{
+    alert("largest is"+n1)
+    }
+else if(n2>n1 && n2>n3)
+{
+    alert("largest is"+n2)
+    }
+else if(n3>n1 && n3>n2)
+{
+    alert("largest is"+n3)
+    }
+
+
+
---
+++ javascript_decision/oddeven.js
@@ -0,0 +1,5 @@
+var num=prompt("enter value");
+if(num%2==0){
+alert("even")}
+else{
+alert("odd")}
